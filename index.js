af54f16624a93c1da3b6944ae6f42ad0d14ea794
Terlan Cavadzade <tarlan.cavadzada.a@asoiu.edu.az>
2024-12-07 21:54:37 +0400

    version 1.0.0

---
+++ index.js
@@ -0,0 +1,47 @@
+const btnGroups = document.querySelectorAll(".button-group");
+const optionListContainer = document.querySelector(".selected-options-list");
+
+btnGroups.forEach((btnGroup) => {
+  btnGroup.addEventListener("click", (e) => {
+    if (e.target.matches(`[aria-role="label"]`)) {
+      const targetParent = e.target.parentNode;
+      const allElementsOfParent = targetParent.querySelectorAll(
+        `[aria-role="label"] > input`
+      );
+      allElementsOfParent.forEach((el) => {
+        el.checked = el.parentNode === e.target;
+      });
+
+      const selectedOptions = getAllSelectedOptions();
+      addOptionsToEl(selectedOptions, "li", optionListContainer);
+    }
+  });
+}, true);
+
+const getAllSelectedOptions = () => {
+  const allSelectedOptions = document.querySelectorAll(
+    `.button-group > [aria-role="label"] > input:checked`
+  );
+
+  const optionNodesArrayForm = Array.from(allSelectedOptions);
+  const selectedValues = optionNodesArrayForm.map(
+    (optionNode) => optionNode.value
+  );
+  return selectedValues;
+};
+
+const addOptionsToEl = (options, el, parentNode) => {
+  //   parentNode.querySelectorAll("*").forEach((el) => {
+  //     el.remove();
+  //   });
+
+  parentNode.innerHTML = "";
+
+  //   both works
+
+  options.forEach((option) => {
+    const createdElement = document.createElement(el);
+    createdElement.textContent = option;
+    parentNode.append(createdElement);
+  });
+};
